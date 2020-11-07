9a5a14aee622a4678e25feea55903f887442e862
Spain-It <tahsingokmen@hotmail.com>
2020-11-07 22:41:05 +0100

    first day

---
+++ src/test/java/com/automation/ApiDemo.java
@@ -0,0 +1,71 @@
+package com.automation;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ApiDemo {
+     /*
+    From the main view, navigate through Preferences -> Preference dependencies
+   •Turn ON WiFi option. Select WiFi Settings and write any text and click OK to close the dialog
+  */
+
+    @Test
+    public void apidemo() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability("platformName","Android");
+        desiredCapabilities.setCapability("platformVersion","7.0");
+        desiredCapabilities.setCapability("deviceName","Pixel_2");
+        desiredCapabilities.setCapability("automationName","UiAutomator2");
+        desiredCapabilities.setCapability("app","C:\\Users\\Tahsin\\IdeaProjects\\AppiumAutomationTesting\\apidemos.apk");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http:localhost:4723/wd/hub"),desiredCapabilities);
+
+        Thread.sleep(5000);
+
+        WebElement preference = driver.findElementByAccessibilityId("Preference");
+        preference.click();
+
+        Thread.sleep(2000);
+        WebElement preferenceDependencies = driver.findElementByAccessibilityId("3. Preference dependencies");
+        preferenceDependencies.click();
+
+        WebElement checkBox = driver.findElementById("android:id/checkbox");
+        checkBox.click();
+
+        Thread.sleep(2000);
+
+        WebElement wifiSetting = driver.findElement(By.xpath("(//*[@class='android.widget.RelativeLayout'])[2]"));
+        wifiSetting.click();
+
+        WebElement inputBox = driver.findElementById("android:id/edit");
+
+        inputBox.sendKeys("appiumTest");
+
+        WebElement okButton = driver.findElementById("android:id/button1");
+
+        Assert.assertEquals("appiumTest",inputBox.getText());
+
+        okButton.click();
+
+        Thread.sleep(3000);
+
+
+        wifiSetting.click();
+
+        Thread.sleep(7000);
+
+        driver.closeApp();
+
+    }
+
+}
--- src/test/java/com/automation/FirstAppiumTest.java
+++ src/test/java/com/automation/FirstAppiumTest.java
@@ -4,6 +4,7 @@ package com.automation;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -17,16 +18,22 @@ public class FirstAppiumTest {
 
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
-        desiredCapabilities.setCapability("plartformName","Android");
+        desiredCapabilities.setCapability("platformName","Android");
         desiredCapabilities.setCapability("platformVersion","7.0");
         desiredCapabilities.setCapability("deviceName","Pixel_2");
         desiredCapabilities.setCapability("automationName","UiAutomator2");
         desiredCapabilities.setCapability("app","C:\\Users\\Tahsin\\IdeaProjects\\AppiumAutomationTesting\\etsy.apk");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http:localhost:4723/wd/hub"),desiredCapabilities);
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http:localhost:4723/wd/hub"),desiredCapabilities);
 
         Thread.sleep(5000);
+
+        WebElement getStarted = driver.findElementById("com.etsy.android:id/btn_link");
+        getStarted.click();
+
+
     }
+
 
 
 
